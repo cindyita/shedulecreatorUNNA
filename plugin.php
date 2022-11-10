76973c2455f7aa4e433a704ec0752838a9873dec
Cindy <cindy_al25@hotmail.com>
2022-11-10 16:00:16 -0600

    cambio de version

--- plugin.php
+++ plugin.php
@@ -4,7 +4,7 @@ Plugin Name: Shedule creator
 Description:  Creador de itinerarios y eventos: Profesores, inscripciones y más.
 Author: Cindy ita
 Author URL: https://www.cindyita.com
-Version: BETA 1.7.1
+Version: BETA 1.7.3
 Text Domain: shedule-creator-by-cindyita
 */
 
